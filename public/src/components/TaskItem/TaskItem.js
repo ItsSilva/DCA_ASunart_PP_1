5c46a3f472c26aa6cbaae44ce062932da5412c9d
Juan Silva <juanse909@hotmail.com>
2024-09-12 12:43:49 -0500

    TaskItem component

---
+++ public/src/components/TaskItem/TaskItem.js
@@ -0,0 +1,43 @@
+class TaskItem extends HTMLElement {
+    constructor(){
+        super();
+        this.attachShadow({mode: 'open'});
+    }
+
+    static get observedAttributes(){
+        return ['title', 'description', 'state'];
+    }
+
+    connectedCallback(){
+        this.render();
+    }
+
+    attributeChangedCallback(propName, oldValue, newValue){
+        this.render()
+        if (oldValue !== newValue) {
+            this[propName] = propName === 'state' ? newValue === 'true' : newValue
+            this.render()
+        }
+    }
+
+    toggleTask(){
+        this.state = !this.state;
+        this.render();
+    }
+
+    render(){
+        this.shadowRoot.innerHTML = `
+        <link rel="stylesheet" href="./src/components/TaskItem/TaskItem.css">
+        <li class=${this.state ? 'completed' : 'task'}>
+            <h3>${this.title}</h3>
+            <p>${this.description}</p>
+            <p>${!this.state ? 'pending' : 'completed'}</p>
+            <input type="checkbox" ${this.state ? 'checked' : ''} class='task-checkbox'>
+        </li>
+        `
+        const checkbox = this.shadowRoot.querySelector('.task-checkbox');
+        checkbox.addEventListener('click', () => this.toggleTask());
+    }
+}
+customElements.define('task-item', TaskItem);
+export default TaskItem;
