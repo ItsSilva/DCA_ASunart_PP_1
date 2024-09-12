09fd4cc27d201c67c8a6eb7762acb530b14c458e
Juan Silva <juanse909@hotmail.com>
2024-09-12 12:44:02 -0500

    TaskList component

---
+++ public/src/components/TaskList/TaskList.js
@@ -0,0 +1,50 @@
+import TaskItem from "../TaskItem/TaskItem.js";
+
+class TaskList extends HTMLElement {
+    constructor(){
+        super();
+        this.attachShadow({mode: 'open'});
+        this.tasks = [];
+    }
+
+    connectedCallback(){
+        this.render();
+        
+        const form = this.shadowRoot.querySelector('.task-form');
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            const title = this.shadowRoot.querySelector('.input-title').value;
+            const description = this.shadowRoot.querySelector('.input-description').value;
+
+            this.tasks.push({title, description, state: false});
+
+            this.addTask({title, description, state: false});
+
+            form.reset();
+        });
+    }
+    render(){
+        this.shadowRoot.innerHTML = `
+            <h2>Task List</h2>
+            <form class='task-form'>
+            <input type='text' placeholder='Add a Task' required class='input-title' />
+            <input type='text' placeholder='Add a Description' required class='input-description' />
+            <button>Add Task</button>
+            </form>
+            <ul class="tasks-container"></ul>
+        `
+        this.tasks.forEach(task => this.addTask(task));
+    }
+
+    addTask({title, description, state}){
+        const taskItem = document.createElement('task-item')
+        taskItem.setAttribute('title', title)
+        taskItem.setAttribute('description', description)
+        taskItem.setAttribute('state', state)
+
+        this.shadowRoot.querySelector('.tasks-container').appendChild(taskItem)
+    }
+}
+customElements.define('task-list', TaskList);
+export default TaskList;
